d28d3e99e33cdd26690c155b60fda554960cab6b
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-06 17:50:24 +0300

    done v2.2.

--- src/js/getSort.js
+++ src/js/getSort.js
@@ -1,9 +1,8 @@
 import compare from './compare';
 import arrowToggle from './arrowToggle';
 
-const tableBody = document.getElementById('tableBody');
-
 export default function getSort(key, direction) {
+  const tableBody = document.getElementById('tableBody');
   const dataSort = Array.from(tableBody.getElementsByTagName('tr'));
   compare(key, direction, dataSort);
 
